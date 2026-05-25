692b897e63a8adf299601a1d56d58987e0c47d2b
geojax <micha.rand@gmail.com>
2026-05-25 16:54:54 -0700

    added raycast interaction to UI

---
+++ Assets/scripts/MenuInteractions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MenuInteractions : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void ChangeSelectedScene(GameObject gameObject)
+    {
+        Debug.Log("SelectedScene was changed");
+    }
+    public void LoadSelectedScene()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
